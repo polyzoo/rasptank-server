d42674b0b66f13b24cd05f16030e07cc8813f6e7
Anton Kimyaev <a.kimyaev@idaproject.com>
2026-02-25 01:19:22 +0300

    fix(DriveControllerProtocol): рефакториг протоколов

--- src/application/protocols.py
+++ src/application/protocols.py
@@ -1,22 +1,34 @@
 from abc import abstractmethod
-from typing import Protocol
+from typing import Protocol, Optional
 
 
-class HealthCheckProtocol(Protocol):
-    """Протокол для проверки готовности сервера к работе."""
+class MotorControllerProtocol(Protocol):
+    """Протокол для абстракции управления двигателями машинки."""
 
     @abstractmethod
-    def check(self) -> None:
-        """Проверка готовности сервера с машинкой к работе."""
+    def move_forward(self, speed_percent: int) -> None:
+        """Движение машинки вперед с заданной скоростью."""
+
+    @abstractmethod
+    def stop(self) -> None:
+        """Немедленная остановка обоих двигателей."""
+
+
+class UltrasonicSensorProtocol(Protocol):
+    """Протокол для работы с ультразвуковым датчиком расстояния машинки."""
+
+    @abstractmethod
+    def measure_distance_cm(self) -> float:
+        """Выполнение измерения и получение расстояния до препятствия в сантиметрах."""
 
 
 class DriveControllerProtocol(Protocol):
-    """Протокол для управления движением машинки."""
+    """Протокол для управления движением машинки на высоком уровне."""
 
     @abstractmethod
-    def forward_cm(self, distance_cm: float, max_speed_percent: float | None = None) -> None:
+    def forward_cm(self, distance_cm: float, max_speed_percent: Optional[int] = None) -> None:
         """Движение машинки вперёд на заданное расстояние с ограничением скорости."""
 
     @abstractmethod
     def stop(self) -> None:
-        """Немедленная остановка машинки."""
+        """Немедленная остановка движения машинки."""
