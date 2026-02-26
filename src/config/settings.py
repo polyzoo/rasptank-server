7138f2803ba89836d5279a94e37a5bc32dafb079
Anton Kimyaev <a.kimyaev@idaproject.com>
2026-02-26 23:00:41 +0300

    fix(Settings): исправление переменных

--- src/config/settings.py
+++ src/config/settings.py
@@ -6,8 +6,8 @@ from pydantic import BaseSettings, Field
 class Settings(BaseSettings):
     """Основные настройки приложения."""
 
-    app_host: str = Field(..., alias="APP_HOST")
-    app_port: int = Field(..., alias="APP_PORT")
+    app_host: str = Field(default="0.0.0.0", alias="APP_HOST")
+    app_port: int = Field(default=8010, alias="APP_PORT")
 
     class Config:
         env_file: str = ".env"
